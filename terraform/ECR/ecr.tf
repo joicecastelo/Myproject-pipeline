e26a3aa071652edac6bed5cdbb678ddea2fab9d1
joice.castelo <joice.castelo@ritain.io>
2023-05-03 23:52:28 +0100

    aplicacao de pepilene

--- terraform/ECR/ecr.tf
+++ terraform/ECR/ecr.tf
@@ -2,6 +2,14 @@
 
 resource "aws_ecr_repository" "my_second_repo" {
   name                 = "my-second-repo" # Nome do meu repositório
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
+  encryption_configuration {
+  encryption_type = "KMS"
+  
+ }
+
+ image_scanning_configuration {
+   scan_on_push = true
+ } 
 }
