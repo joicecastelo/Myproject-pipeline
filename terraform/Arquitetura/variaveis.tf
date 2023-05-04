93b8ed9e3f2d912ad589e94730854a7841c22336
joice.castelo <joice.castelo@ritain.io>
2023-05-04 14:41:10 +0100

    aplicacao de pepilene

--- terraform/Arquitetura/variaveis.tf
+++ terraform/Arquitetura/variaveis.tf
@@ -67,19 +67,3 @@ variable "ecs_task_execution_role" {
   description = "ECS Task Execution Role name"
 }
 
-
-
-
-output "aws_ecr_registry_id" {
-
-value = aws_ecr_repository.my_second_repo.registry_id
-
-}
-
-
-
-output "aws_ecr_repository_url" {
-
- value = aws_ecr_repository.my_second_repo.repository_url
-
-}
