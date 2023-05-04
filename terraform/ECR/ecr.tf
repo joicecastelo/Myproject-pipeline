56cf328ee2615835d6f7ae369f16f2a824623e12
joice.castelo <joice.castelo@ritain.io>
2023-05-04 09:54:50 +0100

    aplicacao de pepilene

--- terraform/ECR/ecr.tf
+++ terraform/ECR/ecr.tf
@@ -10,3 +10,25 @@ resource "aws_ecr_repository" "my_second_repo" {
   name                 = "my-second-repo" # Nome do meu repositório
   
 }
+
+
+################################
+
+#           Outputs            #
+
+################################
+
+output "aws_ecr_registry_id" {
+
+value = aws_ecr_repository.my_second_repo.registry_id
+
+}
+
+
+
+
+output "aws_ecr_repository_url" {
+
+ value = aws_ecr_repository.my_second_repo.repository_url
+
+}
