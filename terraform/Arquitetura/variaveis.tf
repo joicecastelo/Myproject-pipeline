94bb682949e23f36ffcb203aa284b03bc28789a7
joice.castelo <joice.castelo@ritain.io>
2023-05-08 17:34:34 +0100

    Aplicação de pipeline

--- terraform/Arquitetura/variaveis.tf
+++ terraform/Arquitetura/variaveis.tf
@@ -73,33 +73,5 @@ variable "docker_image_name" {}
 
 
 
-# Criar repositorio
-
-resource "aws_ecr_repository" "my_second_repo" {
-  name                 = "my-second-repo" # Nome do meu repositório
-  
-}
-
-
-################################
-
-#           Outputs            #
-
-################################
-
-output "aws_ecr_registry_id" {
-
-value = aws_ecr_repository.my_second_repo.registry_id
-
-}
-
-
-
-
-output "aws_ecr_repository_url" {
-
- value = aws_ecr_repository.my_second_repo.repository_url
-
-}
 
 
