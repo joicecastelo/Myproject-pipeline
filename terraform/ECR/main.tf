81eb7bc11e4d5a1cb3c73ecd6d77b4e064874472
joice.castelo <joice.castelo@ritain.io>
2023-05-15 11:00:35 +0100

    my Api

--- terraform/ECR/main.tf
+++ terraform/ECR/main.tf
@@ -5,6 +5,7 @@ resource "aws_ecr_repository" "my_second_repo" {
   name                 = "my-second-repo" # Nome do meu repositório
 
 
+/*
    image_tag_mutability            = "IMMUTABLE"             # Added after checkov analysis
    image_scanning_configuration {                            # Added after checkov analysis
     scan_on_push = true
@@ -13,6 +14,8 @@ resource "aws_ecr_repository" "my_second_repo" {
     encryption_type                 = "KMS"                   
   }
   #force_delete                    = true
+
+  */
 
   
 }
