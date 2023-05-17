00e6fa707e73ff8675d4c3b0e73651b6db6f34a5
joice.castelo <joice.castelo@ritain.io>
2023-05-17 02:04:23 +0100

    Aplicacao

--- terraform/ECR/main.tf
+++ terraform/ECR/main.tf
@@ -1,21 +1,5 @@
 
 
-terraform {
-  required_version = ">= 1.0.7"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.62.0"
-    }
-  }
-
-
-  backend "s3" {
-    bucket = "mybucketjoice"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
 
 provider "aws" {
   region = "us-east-1"
